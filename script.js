f2c06a24b42d99adb39cca12a55ca14f2f7e780a
sharmababita <sharma.089babita@gmail.com>
2024-10-24 23:52:56 +0200

    JavaScript introduction

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log('Hello!');
+// Variables
+
+// Let variable saving the number 28 on the name age
+let age = 28;
+// Chaining method:
+console.log("Min alder er:" , age);
+// Template litteral:
+console.log(`Min alder er: ${age}`);
+console.log(typeof age);
+
+// Const variable saving the string Julie on the name name.
+const name = "Babita";
+console.log("Mitt navn er:" , name);
+console.log(typeof name);
+
+// Const variable saving the boolean true on the name isPresent
+const isPresent = true;
+console.log("Er tilstede?" , isPresent);
+console.log(typeof isPresent);
+
+// Two const variables saving strings
+// One const variable saving the data from the two first variables
+const firstName = "Babita";
+const lastName = "Sharma";
+const fullName = firstName + " " + lastName;
+console.log("fullName variable viser:" , fullName);
+
+const htmlElement = document.querySelector("#javaScriptData");
+htmlElement.textContent = fullName;
